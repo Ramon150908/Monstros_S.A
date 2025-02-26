cebc7df645c545840a543b44b031e5637b9930a5
Ramon150908 <163332543+Ramon150908@users.noreply.github.com>
2025-02-26 14:54:05 -0300

    ..................................................

---
+++ Monster/code.js
@@ -0,0 +1,15 @@
+
+//JSON
+function banco(){
+    const bd =  [
+                {id:1,login:"john", senha:"1234",nome:"John",email:"john@gmail.com"}, //0
+                {id:2,login:"ringo", senha:"1234@",nome:"Ringo",email:"Ringo@gmail.com"}, //1
+                {id:3,login:"paul", senha:"1234#",nome:"Paul",email:"paul@gmail.com"} //2    
+            ]
+    let dados = JSON.stringify(bd)
+
+    localStorage.setItem("banco", dados)
+
+    console.log(dados)
+}
+
